884826d32cbb5a16b14d042fd7f60da199d16221
Skylar Kelty <S.Kelty@kent.ac.uk>
2013-12-20 14:15:42 +0000

    Clear out the table after synch, also tidy up

--- cli/provision.php
+++ cli/provision.php
@@ -13,15 +13,14 @@ require_once (dirname(__FILE__) . '/../lib/panopto_data.php');
 
 $USER->username = 'moodlesync';
 
-
 $result = array();
+$panopto_data = new panopto_data(null);
 
 // Go through all courses that need updating and provision them
 $courses = $DB->get_records_select("panopto_course_update_list", "courseid");
 foreach ($courses as $course) {
-  $courseid = $course->courseid;
+  $panopto_data->moodle_course_id = $course->courseid;
   try {
-    $panopto_data = new panopto_data($courseid);
     $provisioning_data = $panopto_data->get_provisioning_info();
     
     // Provision the course
@@ -29,15 +28,20 @@ foreach ($courses as $course) {
 
     $result []= array(
       'result' => 'ok',
-      'course' => $courseid
+      'course' => $course->courseid
     );
   } catch( Exception $e ) {
     $result []= array(
       'result' => 'error',
-      'course' => $courseid,
+      'course' => $course->courseid,
       'exception' => $e->getMessage()
     );
   }
 }
+
+// Clear out the DB
+$DB->delete_records_list("panopto_course_update_list", "courseid", array_map(function($course) {
+  return $course->courseid;
+}, $courses));
 
 echo json_encode($result);
