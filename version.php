f55d974372a0e7846005d7587ef02001f9ae5768
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-03-03 15:42:04 +0000

    Up the Panopto version

--- version.php
+++ version.php
@@ -34,7 +34,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014030200;
+$plugin->version   = 2014030300;
 $plugin->requires  = 2013110500; // Version 2.6
 $plugin->cron      = 600; // 10 minutes
 $plugin->maturity  = MATURITY_STABLE;
