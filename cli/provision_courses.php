47e07aa8e341513fb71da573256e0a92ea1fcf0a
Skylar Kelty <s.kelty@kent.ac.uk>
2015-11-03 15:28:39 +0000

    Support single course syncs

--- cli/provision_courses.php
+++ cli/provision_courses.php
@@ -6,26 +6,38 @@
 define('CLI_SCRIPT', true);
 
 require_once(dirname(__FILE__) . '/../../../config.php');
+require_once($CFG->libdir . '/clilib.php');
 require_once($CFG->libdir . '/accesslib.php');
 require_once(dirname(__FILE__) . '/../lib/panopto_data.php');
 
+list($options, $unrecognized) = cli_get_params(
+    array(
+        'course' => '*',
+    )
+);
+
 $USER->username = 'moodlesync';
 
-$result = array();
+// Build list of users.
+$courses = array();
+if ($options['course'] == '*') {
+    $courses = $DB->get_fieldset_select('block_panopto_foldermap', 'moodleid', '');
+} else {
+    $courses = explode(',', $options['course']);
+}
 
-// Go through all courses that need updating and provision them
-$courses = $DB->get_records("block_panopto_foldermap");
+// Go through courses that need updating and provision them.
 foreach ($courses as $course) {
-    $panoptodata = new panopto_data($course->moodleid);
+    $panoptodata = new panopto_data($course);
 
     try {
         // Provision the course.
         $provisioningdata = $panoptodata->get_provisioning_info();
         $panoptodata->provision_course($provisioningdata);
 
-        echo "Provisioned {$course->moodleid}\n";
+        echo "Provisioned {$course}\n";
     } catch (Exception $e) {
-        echo "Error provisioning {$course->moodleid}...\n";
+        echo "Error provisioning {$course}...\n";
         echo $e->getMessage();
         echo "\n";
     }
